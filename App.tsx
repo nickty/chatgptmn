8528a40f15f290fa895a4908d113480f8b45b48a
Mizanur Rahman <mizan@arogga.net>
2023-07-10 11:26:33 +0600

    With deployment setup first part

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -33,6 +33,7 @@ type SectionProps = PropsWithChildren<{
 
 function Section({children, title}: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+
   return (
     <View style={styles.sectionContainer}>
       <Text
@@ -64,20 +65,53 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [inputMessage, setInputMessage] = useState('');
+  const [outputMessage, setOutputMessage] = useState("Results to be shown here");
+
+  const handleButtonClick = () => {
+    console.log('clicked');
+    console.log(inputMessage);
+    fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'Bearer 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',
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: inputMessage,
+      }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        setOutputMessage(data.choices[0].text.trim())
+        console.log(data.choices[0].text)
+      });
+  };
+  //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
+  const handleTextInput = text => {
+    // console.log("first", text)
+    setInputMessage(text);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={{flex: 1, justifyContent: 'center', marginBottom: 20}}>
-        <Text>Results here:</Text>
+        <Text>{outputMessage}</Text>
       </View>
-      <View style={{flexDirection: 'row'}}>
+      <View style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
         <View
-          style={{flex: 1, marginLeft: 10, marginRight: 10, marginBottom: 20}}>
-          <TextInput placeholder="Enter your questions" />
+          style={{ borderWidth: 1, borderColor: '#ccc', borderRadius: 10, padding: 15}}>
+          <TextInput
+            onChangeText={handleTextInput}
+            placeholder="Enter your questions"
+          />
         </View>
 
-        <TouchableOpacity>
-          <View style={{backgroundColor: 'red', padding: 5}}>
-            <Text>Send</Text>
+        <TouchableOpacity onPress={handleButtonClick} style={{padding: 20}}>
+          <View style={{backgroundColor: 'green', padding: 15, borderRadius: 10}}>
+            <Text style={{ color: 'white'}}>Send</Text>
           </View>
         </TouchableOpacity>
       </View>
